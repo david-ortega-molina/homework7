37468b543a83e1722e971aefb73979a8145e9015
david-ortega-molina <63212572+david-ortega-molina@users.noreply.github.com>
2021-06-01 22:39:22 -0700

    test is successful because output and testing =

--- hw7cs362/leaptest.py
+++ hw7cs362/leaptest.py
@@ -3,7 +3,7 @@ import leap
 
 class testCase(unittest.TestCase):
     def test1(self):
-            self.assertEqual(leap.leapyr(2011),  (2011, "is a leap year!!"))
+            self.assertEqual(leap.leapyr(2020),  (2020, "is a leap year!!"))
 
 if __name__ == "__main__":
 
